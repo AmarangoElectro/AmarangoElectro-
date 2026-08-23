853427c26a2df48574c3683c344b517530c5738f
AmarangoElectro <max.huracan73@gmail.com>
2026-08-23 14:24:21 -0300

    Activa acciones de admin sin saltos de pantalla

--- public/margarita-ui.js
+++ public/margarita-ui.js
@@ -2,3 +2,4 @@ export * from "./margarita-ui-core.js";
 import "./tienda-optimizaciones.js";
 import "./margarita-identidad.js";
 import "./margarita-query-guard.js?v=20260823-2";
+import "./admin-stable-actions.js?v=20260823-1";
