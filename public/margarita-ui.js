a28292bcca568f9ba17394c0611630b46c0468e5
AmarangoElectro <max.huracan73@gmail.com>
2026-08-23 19:38:43 -0300

    Activa acceso visible a Equipo de ventas

--- public/margarita-ui.js
+++ public/margarita-ui.js
@@ -1,5 +1,6 @@
 export * from "./margarita-ui-core.js";
 import "./tienda-optimizaciones.js";
 import "./margarita-identidad.js";
+import "./equipo-ventas-menu.js?v=20260823-1";
 import "./margarita-query-guard.js?v=20260823-2";
 import "./admin-stable-actions.js?v=20260823-1";
