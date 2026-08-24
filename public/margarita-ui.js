9f20cf997f806c9b08e11c1a24f93bf81e01b986
AmarangoElectro <max.huracan73@gmail.com>
2026-08-24 10:52:06 -0300

    Conecta Margarita premium al cerebro V18 solo para asesores

--- public/margarita-ui.js
+++ public/margarita-ui.js
@@ -4,3 +4,6 @@ import "./margarita-identidad.js";
 import "./equipo-ventas-menu.js?v=20260823-1";
 import "./margarita-query-guard.js?v=20260823-2";
 import "./admin-stable-actions.js?v=20260823-1";
+import "./margarita-v18-bridge.js?v=20260824-1";
+import "./margarita-asesor-copy.js?v=20260824-1";
+import "./margarita-nueva-consulta.js?v=20260824-1";
