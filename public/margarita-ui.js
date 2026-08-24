7089fa35add555e6e6c374f874671cebfb921128
AmarangoElectro <max.huracan73@gmail.com>
2026-08-24 10:01:36 -0300

    Revert urgente: restaura Margarita al último estado estable

--- public/margarita-ui.js
+++ public/margarita-ui.js
@@ -2,6 +2,5 @@ export * from "./margarita-ui-core.js";
 import "./tienda-optimizaciones.js";
 import "./margarita-identidad.js";
 import "./equipo-ventas-menu.js?v=20260823-1";
-import "./asesor-registrar-venta.js?v=20260823-1";
 import "./margarita-query-guard.js?v=20260823-2";
 import "./admin-stable-actions.js?v=20260823-1";
