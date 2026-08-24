b7758b6030c0a49b1be89568f899cfd227bd68d6
AmarangoElectro <max.huracan73@gmail.com>
2026-08-24 19:38:18 -0300

    Carga Margarita guía simplificada para clientes

--- public/margarita-ui.js
+++ public/margarita-ui.js
@@ -4,3 +4,4 @@ import "./margarita-identidad.js";
 import "./equipo-ventas-menu.js?v=20260823-1";
 import "./margarita-query-guard.js?v=20260823-2";
 import "./admin-stable-actions.js?v=20260823-1";
+import "./margarita-guia-clientes.js?v=20260824-1";
