6dfe888516edfd25dc5c27d7f016345013ab2f55
AmarangoElectro <max.huracan73@gmail.com>
2026-09-21 08:55:36 -0300

    feat: expand sanitized cellphone batch

--- lib/catalog/v16-cellphones-sanitized-snapshot-adapter.ts
+++ lib/catalog/v16-cellphones-sanitized-snapshot-adapter.ts
@@ -32,7 +32,7 @@ const evidenceSchema = z.object({
 
 const parsedEvidence = evidenceSchema.parse(evidence);
 
-const APPROVED_POSITIONS = new Set([0, 4, 16, 17, 44, 45]);
+const APPROVED_POSITIONS = new Set([0, 1, 2, 4, 5, 6, 16, 17, 19, 20, 21, 22, 23, 24, 25, 44, 45, 46]);
 
 function slugPart(value: string) {
   return value
