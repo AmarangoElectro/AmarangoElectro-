cb11572005d558292b0a01b133d2b04290ef8165
AmarangoElectro <max.huracan73@gmail.com>
2026-09-20 23:06:25 -0300

    config: allow audited external catalog image CDN

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         hostname: "zctaukyrhsmpjkcddcqq.supabase.co",
         pathname: "/storage/v1/object/public/tienda-fotos/**",
       },
+      {
+        protocol: "https",
+        hostname: "cdn.catalog-store.link",
+        pathname: "/**",
+      },
     ],
   },
 };
