33e400722bc4d1a4b5e250a07ae7f5755f808b29
AmarangoElectro <max.huracan73@gmail.com>
2026-09-22 18:09:35 -0300

    Mi Balance: refresh cache for history edit and backup

--- prototypes/mi-balance-v1/service-worker.js
+++ prototypes/mi-balance-v1/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mi-balance-v18-long-goals-currency-trash-feedback';
+const CACHE='mi-balance-v19-monthly-edit-backup';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./icon-maskable.svg','./social-preview-v2.svg'];
 
 self.addEventListener('install',event=>{
