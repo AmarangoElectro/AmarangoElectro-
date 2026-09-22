88456b887821027426c3639e66a436075ba808ef
AmarangoElectro <max.huracan73@gmail.com>
2026-09-22 15:27:45 -0300

    Mi Balance: refresh cache for guide fix

--- prototypes/mi-balance-v1/service-worker.js
+++ prototypes/mi-balance-v1/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mi-balance-v6-guide-session-sounds-monitor';
+const CACHE='mi-balance-v7-guide-five-steps';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./icon-maskable.svg','./social-preview-v2.svg'];
 
 self.addEventListener('install',event=>{
