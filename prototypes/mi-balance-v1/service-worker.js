dccafe08769b65b96a1197706ed7f4186a24aa3f
AmarangoElectro <max.huracan73@gmail.com>
2026-09-22 13:45:53 -0300

    Mi Balance: refresh service worker for install update

--- prototypes/mi-balance-v1/service-worker.js
+++ prototypes/mi-balance-v1/service-worker.js
@@ -1,1 +1,43 @@
-const C='mi-balance-v1';const A=['./','./index.html','./manifest.json','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='mi-balance-v2-install-share';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./icon-maskable.svg','./social-preview-v2.svg'];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key.startsWith('mi-balance-')&&key!==CACHE).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET')return;
+
+  if(req.mode==='navigate'){
+    event.respondWith(
+      fetch(req)
+        .then(res=>{
+          const copy=res.clone();
+          caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
+          return res;
+        })
+        .catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(req).then(hit=>hit||fetch(req).then(res=>{
+      if(new URL(req.url).origin===self.location.origin&&res.ok){
+        const copy=res.clone();
+        caches.open(CACHE).then(cache=>cache.put(req,copy));
+      }
+      return res;
+    }))
+  );
+});
