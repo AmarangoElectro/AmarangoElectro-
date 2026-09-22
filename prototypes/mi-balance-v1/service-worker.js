44bd2420204954a96cf9c92b0e58dbd5eb1303a2
AmarangoElectro <max.huracan73@gmail.com>
2026-09-22 16:57:57 -0300

    Mi Balance: refresh cache for seamless startup

--- prototypes/mi-balance-v1/service-worker.js
+++ prototypes/mi-balance-v1/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mi-balance-v15-amarango-referral';
+const CACHE='mi-balance-v16-seamless-session-start';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./icon-maskable.svg','./social-preview-v2.svg'];
 
 self.addEventListener('install',event=>{
