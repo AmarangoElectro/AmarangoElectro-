53d6ee48e937b908c9d3b0cf27b873112991944d
AmarangoElectro <max.huracan73@gmail.com>
2026-09-22 16:06:36 -0300

    Mi Balance: refresh cache for theme and quick-entry redesign

--- prototypes/mi-balance-v1/service-worker.js
+++ prototypes/mi-balance-v1/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mi-balance-v11-recurring-expenses';
+const CACHE='mi-balance-v12-theme-quick-goal-poster';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./icon-maskable.svg','./social-preview-v2.svg'];
 
 self.addEventListener('install',event=>{
