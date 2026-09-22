f190f24630c9de57c70e61dad96c8dbfd9f64014
AmarangoElectro <max.huracan73@gmail.com>
2026-09-22 15:28:45 -0300

    Mi Balance: refresh cache for persistent session

--- prototypes/mi-balance-v1/service-worker.js
+++ prototypes/mi-balance-v1/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mi-balance-v7-guide-five-steps';
+const CACHE='mi-balance-v8-persistent-session';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./icon-maskable.svg','./social-preview-v2.svg'];
 
 self.addEventListener('install',event=>{
