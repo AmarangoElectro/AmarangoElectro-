b34c1ee8a16b00a231199fe32f7ce9de6369a6df
AmarangoElectro <max.huracan73@gmail.com>
2026-09-22 17:08:16 -0300

    Mi Balance: refresh cache for recurring expense correction

--- prototypes/mi-balance-v1/service-worker.js
+++ prototypes/mi-balance-v1/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mi-balance-v16-seamless-session-start';
+const CACHE='mi-balance-v17-quick-minus-one';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./icon-maskable.svg','./social-preview-v2.svg'];
 
 self.addEventListener('install',event=>{
