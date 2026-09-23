5136052fa9d9d1af802bcb0ca8adb065e5ebcf41
AmarangoElectro <max.huracan73@gmail.com>
2026-09-23 19:31:57 -0300

    Mi Balance: refresh cache for Helados and compact history

--- prototypes/mi-balance-v1/service-worker.js
+++ prototypes/mi-balance-v1/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mi-balance-v26-quick-menu-categories';
+const CACHE='mi-balance-v27-helados-history-collapse';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./icon-maskable.svg','./social-preview-v2.svg'];
 
 self.addEventListener('install',event=>{
