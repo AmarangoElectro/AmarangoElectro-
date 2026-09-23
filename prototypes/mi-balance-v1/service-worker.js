002232178bffae8e199e74f5f3409f311543eadd
AmarangoElectro <max.huracan73@gmail.com>
2026-09-23 20:08:53 -0300

    Mi Balance: refresh cache for awareness panel and multi-goals

--- prototypes/mi-balance-v1/service-worker.js
+++ prototypes/mi-balance-v1/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mi-balance-v28-gamified-finance-status';
+const CACHE='mi-balance-v29-money-awareness-multigoals';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./icon-maskable.svg','./social-preview-v2.svg'];
 
 self.addEventListener('install',event=>{
