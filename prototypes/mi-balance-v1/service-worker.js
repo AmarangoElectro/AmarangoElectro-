90070609ecc9ee952ac9904f02fe1d68b8c0db6d
AmarangoElectro <max.huracan73@gmail.com>
2026-09-23 12:34:27 -0300

    Mi Balance: refresh cache for quick menu and categories

--- prototypes/mi-balance-v1/service-worker.js
+++ prototypes/mi-balance-v1/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mi-balance-v25-supermercado-almacen';
+const CACHE='mi-balance-v26-quick-menu-categories';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./icon-maskable.svg','./social-preview-v2.svg'];
 
 self.addEventListener('install',event=>{
