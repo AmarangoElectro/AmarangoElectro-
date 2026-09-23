5a39a31cb8caddfff867ba1d7b6951c734d357d0
AmarangoElectro <max.huracan73@gmail.com>
2026-09-22 22:34:01 -0300

    Mi Balance: refresh cache for Amarango theme tokens

--- prototypes/mi-balance-v1/service-worker.js
+++ prototypes/mi-balance-v1/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mi-balance-v23-amarango-theme-coherence';
+const CACHE='mi-balance-v24-amarango-theme-tokens';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./icon-maskable.svg','./social-preview-v2.svg'];
 
 self.addEventListener('install',event=>{
